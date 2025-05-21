40995ecb34940227fc1358742563599e64f05df5
Eriton <up202008859@up.pt>
2025-05-21 12:39:08 +0100

    w-full on the collections section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,9 +39,9 @@
         <ProductSlider/>
     </section>
     
-    <section id="Collections-section" class="">
+    <section id="Collections-section" class="w-full">
       
-            <h2 class=" text-2xl text-center lg:text-3xl bg-[#fae0df]">You might like</h2>
+            <h2 class="p-4 text-2xl text-center lg:text-3xl bg-[#fae0df]">You might like</h2>
     
             <div class="p-2 grid grid-cols-2 lg:flex justify-center lg:gap-5">
                 <Collection link="shop/collections/earth" url="images/earth-collection-candle.jpeg" caption="Earth"/>
