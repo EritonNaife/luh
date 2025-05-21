02a24884418ff2ce21fee2823f3119d7f4d7a79f
Eriton <up202008859@up.pt>
2025-05-21 22:30:58 +0100

    .

--- src/routes/shop/collections/gift/+page.svelte
+++ src/routes/shop/collections/gift/+page.svelte
@@ -22,24 +22,17 @@
 
     <section class="hero-section" in:scale={{duration: 1000, start: 1.5 , opacity: 0 }} >
 
-        <div class="h-[70vh] flex justify-center items-end"  >
+        <div class="h-[50vh] flex justify-center items-end"  >
             <img src="/images/gift.jpeg" alt="Calm Collection" class="w-full h-full object-cover">
             <h1 class="absolute text-3xl text-white lg:text-5xl mb-20" in:fly={{duration:2000, y:100}} >Gift Candle Collection</h1>
         </div>
 
-        <!--
 
-            <div class="p-5 flex flex-col justify-center items-center">
-                <p class="text-center text-2xl/8 font-light lg:text-xl">
+        <div class="p-5 flex flex-col justify-center items-center">
+            <p class="text-center text-2xl/8 font-light lg:text-xl">
                     Wrapped in intention, the Gift Collection turns scent into sentiment. Delicate florals, warm resins, and bright botanicals are presented in timeless vessels—ready to be treasured. Whether marking a milestone or offering everyday elegance, these candles elevate the art of giving. Thoughtful, refined, and universally loved, they speak without words and linger with grace.</p>
-            </div>
-        -->
-
-        <div class="p-4 flex flex-col justify-center items-center gap-2" in:fly={{duration:2000, y:100}}>
-            <h2 class="text-2xl/8 font-light lg:text-2xl">Return to Stillness</h2>
-            <p class="text-center text-md/8 font-light lg:text-xl">Soft chamomile, white lavender, and clean woods bring peace to the moment and quiet to the mind</p>
         </div>
-        
+    
 
     </section>
     
