d43624d81c52a327ffe0fb40540951ab33a1247e
Eriton <up202008859@up.pt>
2025-05-21 15:40:53 +0100

    w-[55vw]

--- src/lib/Components/Cards/Collection.svelte
+++ src/lib/Components/Cards/Collection.svelte
@@ -22,7 +22,7 @@
 
 -->
 
-<a href={link} class="relative w-[50vw] md:w-[48vw] lg:w-[24vw] group cursor-pointer flex flex-col items-center">
+<a href={link} class="relative w-[55vw] md:w-[48vw] lg:w-[24vw] group cursor-pointer flex flex-col items-center">
 
     <header class="aspect-[3/4] w-full">
         <img src={url} alt="" class="w-full h-full object-cover transition-trasnform duration-700 group-hover:scale-103">
