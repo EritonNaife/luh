e2490d24bab5170b584ff74174e5d4bf5b3a67ce
Eriton <up202008859@up.pt>
2025-05-21 21:39:06 +0100

    added animations

--- src/routes/shop/collections/earth/+page.svelte
+++ src/routes/shop/collections/earth/+page.svelte
@@ -20,14 +20,14 @@
 
 <main>
 
-    <section class="hero-section">
+    <section class="hero-section" in:scale={{duration: 1000, start: 1.5 , opacity: 0 }}>
 
-        <div class="h-[70vh] flex justify-center items-end sm:h-[80vh] md:h-[70vh] lg:h-[80vh]" in:scale={{duration: 1000, start: 1.5 , opacity: 0 }} >
+        <div class="h-[70vh] flex justify-center items-end sm:h-[80vh] md:h-[70vh] lg:h-[80vh]"  >
             <img src="/images/earth.jpeg" alt="http://localhost:5173/images/earth.jpegCalm Collection" class="w-full h-full object-cover">
             <h1 class="absolute text-3xl text-white lg:text-5xl mb-20" in:fly={{duration:2000, y:100}} >Earth Candle Collection</h1>
         </div>
 
-        <div class="p-4 flex flex-col justify-center items-center gap-2">
+        <div class="p-4 flex flex-col justify-center items-center gap-2" in:fly={{duration:2000, y:100}} >
             <h2 class="text-2xl/8 font-light lg:text-2xl">Bring the Forest Home</h2>
             <p class="text-center text-md/8 font-light lg:text-xl">Ground yourself in nature’s finest: warm cedarwood, fresh eucalyptus, and rich vetiver to reconnect mind and space</p>
         </div>
