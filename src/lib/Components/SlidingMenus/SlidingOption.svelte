6fa4b53dbd4a794c55275f1629702b270fbf4dc5
Eriton <up202008859@up.pt>
2025-05-30 21:25:51 +0100

    added shadow-[0px_6px_15px_5px_rgba(51,51,51,0.8)]

--- src/lib/Components/SlidingMenus/SlidingOption.svelte
+++ src/lib/Components/SlidingMenus/SlidingOption.svelte
@@ -7,7 +7,7 @@
 
 
 
-<div class="fixed py-4 bottom-0 w-full h-[24vh] bg-white inset-shadow-2xs inset-shadow-black flex flex-col justify-center items-center gap-6" 
+<div class="fixed py-4 bottom-0 w-full h-[24vh] bg-white shadow-[0px_6px_15px_5px_rgba(51,51,51,0.8)] flex flex-col justify-center items-center gap-6" 
      transition:slide = {{duration:300, axis: 'y' ,easing: cubicInOut}}>
 
     <h2 class="text-xl">Choose an Option</h2>
