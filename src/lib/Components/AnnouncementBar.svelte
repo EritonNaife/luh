0f91827c6b5870d60e1583fe62e4d793f101e33e
Eriton <up202008859@up.pt>
2025-07-07 18:44:53 +0100

    .

--- src/lib/Components/AnnouncementBar.svelte
+++ src/lib/Components/AnnouncementBar.svelte
@@ -5,7 +5,7 @@
 <div class="flex w-full overflow-hidden bg-white text-[#2F2F2F] ">
   <div class="animate-marquee flex min-w-full shrink-0 items-center gap-5 py-[2px] whitespace-nowrap">
     {#each announcements as announcement}
-      <p class="flex items-center gap-6 px-2 text-[12px]">{announcement}</p>
+      <p class="flex items-center gap-6 px-2 text-[11px]">{announcement}</p>
     {/each}
   </div>
 </div>
