543b5c0f6f7d15a1133d8c6b003aad575a600142
Eriton <up202008859@up.pt>
2025-07-09 15:41:30 +0100

    .

--- src/lib/Components/AnnouncementBar.svelte
+++ src/lib/Components/AnnouncementBar.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    let announcements = ["welcome","Worldwide shipping","shipping from 60€ to Portugal continental"];
+    let announcements = ["welcome","Worldwide shipping","Free shipping from 60€ to Portugal continental"];
 </script>
 
 <div class="flex w-full overflow-hidden bg-white text-[#2F2F2F] ">
-  <div class="animate-marquee flex min-w-full shrink-0 items-center gap-5 py-[2px] whitespace-nowrap">
+  <div class="animate-marquee flex min-w-full shrink-0 items-center gap-150 py-[2px] whitespace-nowrap">
     {#each announcements as announcement}
-      <p class="flex items-center gap-6 px-2 text-[10px] uppercase">{announcement}</p>
+      <p class="flex items-center gap-6 px-2 text-[11px] uppercase">{announcement}</p>
     {/each}
   </div>
 </div>
