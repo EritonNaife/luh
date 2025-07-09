bf8bd230ddb4d7beea291d3a40109124b7ccfe03
Eriton <up202008859@up.pt>
2025-07-09 15:46:35 +0100

    .

--- src/lib/Components/AnnouncementBar.svelte
+++ src/lib/Components/AnnouncementBar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex w-full overflow-hidden bg-white text-[#2F2F2F] ">
-  <div class="animate-marquee flex min-w-full shrink-0 items-center gap-2 lg:gap-150 py-[2px] whitespace-nowrap">
+  <div class="animate-marquee flex min-w-full shrink-0 items-center gap-10 lg:gap-150 py-[2px] whitespace-nowrap">
     {#each announcements as announcement}
       <p class="flex items-center gap-6 px-2 text-[11px] uppercase">{announcement}</p>
     {/each}
